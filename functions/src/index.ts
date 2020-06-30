73b7a236d8e8469f3708f819212841184ba8a9f7
yuiseki <yuiseki@gmail.com>
2020-06-30 21:06:16 +0900

    認証まわりの修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
-//import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+//const Twitter = require('twit');
+admin.initializeApp(functions.config().firebase);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  /*
+  const client = new Twitter({
+      consumer_key: 'oYaba1q10k8ijD1vR5tncg',
+      consumer_secret: '13AhPHJeIjzjeeOP6840LgsQRouslHtqdDlGU4LHTuw',
+      access_token: '2158391-eWL3LmpmSO65pz8bS9BYG2fXNQO2QWdW5By04ysW19',
+      access_token_secret: 'Z3Bst8LjRWHIWF2xwgHnCVeQMFpUdlDwzz7Wfs29ZRvxUx'
+  });
+  */
+  const idToken = request.query.idToken
+  admin.auth().verifyIdToken(idToken)
+    .then(function(decodedToken: { uid: any; }) {
+      let uid = decodedToken.uid;
+      console.log("uid: "+uid);
+      admin.auth().getUser(uid)
+        .then(function(userRecord: any) {
+          console.log('Successfully fetched user data:', userRecord.toJSON());
+        })
+        .catch(function(error: any) {
+          console.log('Error fetching user data:', error);
+        });
+      response.send(uid);
+    }).catch(function(error: any) {
+      console.log(error)
+    });
+});
+
